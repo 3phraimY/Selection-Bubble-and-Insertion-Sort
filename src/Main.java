5729e0c8e4b48dab398a71736881663a3bcd3eae
3phraimY <ephraim.young@cuw.edu>
2023-09-29 19:32:29 -0500

    hw

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,23 @@ public class Main {
     public static void main(String[] args) {
         // Create an array of random integers
         int[] unsorted = createArray(1000);
+        
+        int[] unsorted2 = unsorted;
+        int[] unsorted3 = unsorted;
+        for(int i=0; i<unsorted.length; i++)
+        {
+        	unsorted2[i] = unsorted[i];
+        	unsorted3[i] = unsorted[i];
+        }
+        
 
         // Sort the array using bubble sort
         bubbleSort(unsorted);
 
         // Sort the array using selection sort
-        selectionSort(unsorted);
+        selectionSort(unsorted2);
+        
+        insertionSort(unsorted3);
     }
 
     //fills an array with provided length with int
@@ -77,5 +88,25 @@ public class Main {
         long timeElapsed = finish - start;
         System.out.println(" a random array size of " + array.length + " took Selection Sort " + timeElapsed + " ns to complete.");
         
+    }
+    public static void insertionSort(int[] array)
+    {
+    	long start = System.nanoTime();
+    	//how many times to run
+    	for(int i = 1; i<array.length; i++)
+    	{
+    		int j=i;
+    		//performs swaps until value no longer smaller
+    		while(array[j] < array[j-1] && j>0)
+    		{
+    			int temp = array[j];
+    			array[j] = array[j-1];
+    			array[j-1] = temp;
+    			j--;
+    		}
+      	}
+    	long finish = System.nanoTime();
+        long timeElapsed = finish - start;
+    	System.out.println(" a random array size of " + array.length + " took Insertion Sort " + timeElapsed + " ns to complete.");
     }
 }
